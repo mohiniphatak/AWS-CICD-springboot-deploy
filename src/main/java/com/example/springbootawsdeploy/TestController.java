9a4834f5d5866694450b2906cb00be9b56971e61
mohiniphatak <phatakmohini@gmail.com>
2024-02-14 12:42:16 +0530

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -16,4 +16,9 @@ public class TestController {
     public String getMessage() {
         return "Second message from AWS Ecs";
     }
+
+    @GetMapping("/mahi")
+    public String getMessage() {
+        return "deployed again";
+    }
 }
